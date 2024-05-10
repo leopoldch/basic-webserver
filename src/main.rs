03ed226972d8f86d1e42272c0fc4cb6551fb3dee
leo <leopold@lchappuis.fr>
2024-05-10 20:36:37 +0200

    multi

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use std::{
     io::{prelude::*, BufReader},
     net::{TcpListener, TcpStream},
+    thread,
+    time::Duration,
 };
 
 use typed_html::{
@@ -9,15 +11,16 @@ use typed_html::{
     types::Metadata,
 };
 
-
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:3000").unwrap();
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        handle_connection(stream);
-    }
 
+        thread::spawn(|| {
+            handle_connection(stream);
+        });
+    }
 }
 
 fn handle_connection(mut stream: TcpStream) {
@@ -25,47 +28,74 @@ fn handle_connection(mut stream: TcpStream) {
 
     let request_line: String = buf_reader.lines().next().unwrap().unwrap();
 
-    let (status_line, contents) = if request_line == "GET / HTTP/1.1" {
-        let doc: DOMTree<String> = html!(
-            <html>
-                <head>
-                    <title>"Hello"</title>
-                    <meta name=Metadata::Author />
-                </head>
-                <body>
-                    <h1>"Hello from Rust"</h1>
-                    <p >
-                        "Hello !"
-                    </p>
-                </body>
-            </html>
-        );
-        let hello = doc.to_string();
+
+
+    let (status_line, contents) = match &request_line[..] {
+        "GET / HTTP/1.1" => {
+            let doc: DOMTree<String> = html!(
+                <html>
+                    <head>
+                        <title>"Hello!"</title>
+                        <meta name=Metadata::Author />
+                    </head>
+                    <body>
+                        <h1>"Hello from Rust !"</h1>
+                        <p >
+                            "Page served using Rust"
+                        </p>
+                    </body>
+                </html>
+            );
+            let hello = doc.to_string();
             ("HTTP/1.1 200 OK", hello)
-    } else {
-        let doc: DOMTree<String> = html!(
-            <html>
-                <head>
-                    <title>"Unauthorized"</title>
-                    <meta name=Metadata::Author/>
-                </head>
-                <body>
-                    <h1>"UNAUTHORIZED"</h1>
-                    <p>
-                        "401 - UNAUTHORIZED"
-                    </p>
-                </body>
-            </html>
-        );
-        let response = doc.to_string();
-        ("HTTP/1.1 401 UNAUTHORIZED", response)
+        },
+        "GET /sleep HTTP/1.1" => {
+            thread::sleep(Duration::from_secs(5));
+            let doc: DOMTree<String> = html!(
+                <html>
+                    <head>
+                        <title>"Sleep"</title>
+                        <meta name=Metadata::Author />
+                    </head>
+                    <body>
+                        <h1>"Sleeping"</h1>
+                        <p >
+                            "Sleep !"
+                        </p>
+                    </body>
+                </html>
+            );
+            let sleep = doc.to_string();
+            ("HTTP/1.1 200 OK", sleep)
+        }
+        _ => {
+
+            let doc: DOMTree<String> = html!(
+                <html>
+                    <head>
+                        <title>"NOT FOUND"</title>
+                        <meta name=Metadata::Author/>
+                    </head>
+                    <body>
+                        <h1>"NOT FOUND"</h1>
+                        <p>
+                            "404 - NOT FOUND"
+                        </p>
+                    </body>
+                </html>
+            );
+            let response = doc.to_string();
+            ("HTTP/1.1 404 NOT FOUND", response)
+        
+        },
     };
 
-    let length = contents.len();
+    
+    let length: usize = contents.len();
 
     let response =
         format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
 
     stream.write_all(response.as_bytes()).unwrap();
-}
 
+}
